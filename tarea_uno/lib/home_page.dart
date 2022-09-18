a7e9a5d416f6ec851170a8203e745c3bb1820904
KAMZ13 <is711272@iteso.mx>
2022-09-18 03:59:30 -0500

    home page created
    Created Home page file and added some widgets

---
+++ tarea_uno/lib/home_page.dart
@@ -0,0 +1,26 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool? _selected;
+  void _reset() {
+    _selected = null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mc Flutter"),
+        backgroundColor: Colors.teal,
+      ),
+    );
+  }
+}
